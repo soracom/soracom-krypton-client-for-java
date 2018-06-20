3202c4b4dad9d6ea4526de1fa5ca0a0a70426839
c9katayama <c95029@gmail.com>
2018-06-20 18:36:40 +0900

    add license text

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # soracom-krypton-client-for-java
 Java based client library for SORACOM Krypton 
+
+
+The soracom-krypton-client-for-java is released under version 2.0 of Apache License
--- src/main/java/io/soracom/krypton/beans/AppkeyBean.java
+++ src/main/java/io/soracom/krypton/beans/AppkeyBean.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2018 SORACOM, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.soracom.krypton.beans;
 
 import com.google.gson.Gson;
@@ -5,7 +19,6 @@ import com.google.gson.GsonBuilder;
 
 public class AppkeyBean {
 	private String applicationKey;
-	
 
 	public String getApplicationKey() {
 		return applicationKey;
@@ -15,18 +28,18 @@ public class AppkeyBean {
 		this.applicationKey = applicationKey;
 	}
 
-	public String toJson(){
+	public String toJson() {
 		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 		return gson.toJson(this);
 	}
-	
-	public static AppkeyBean fromJson(String json){
-		
+
+	public static AppkeyBean fromJson(String json) {
+
 		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 		return gson.fromJson(json, AppkeyBean.class);
 	}
-	
-	public boolean isEmpty(){
-		return (applicationKey==null);
+
+	public boolean isEmpty() {
+		return (applicationKey == null);
 	}
 }
